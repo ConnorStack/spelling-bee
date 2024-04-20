91e702e825974e1d26b279d6c5e9a87ba38d8230
Connor Stack <connorjstack@DESKTOP-H7F119K.localdomain>
2024-04-19 22:34:27 -0500

    Create getters/setters for Word class

--- src/main/java/com/personalproject/spellingbee/model/Word.java
+++ src/main/java/com/personalproject/spellingbee/model/Word.java
@@ -3,4 +3,20 @@ package com.personalproject.spellingbee.model;
 public class Word {
     private Long wordId;
     private String correctWord;
+
+    public Long getId() {
+        return wordId;
+    }
+
+    public void setId(Long id) {
+        wordId = id;
+    }
+
+    public String getCorrectWord() {
+        return correctWord;
+    }
+
+    public void setCorrectWord(String correctWord) {
+        this.correctWord = correctWord;
+    }
 }
