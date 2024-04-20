3a228c946babea6859885c8c69be7f3f421bb3f6
Connor Stack <connorjstack@DESKTOP-H7F119K.localdomain>
2024-04-19 22:10:34 -0500

    Add import statements for REST

--- src/main/java/com/personalproject/spellingbee/controller/WordController.java
+++ src/main/java/com/personalproject/spellingbee/controller/WordController.java
@@ -1,4 +1,9 @@
 package com.personalproject.spellingbee.controller;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class WordController {
 }
