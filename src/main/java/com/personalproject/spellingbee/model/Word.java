6ab819543487538fff7de39a04b5692c07c9eb99
Connor Stack <connorjstack@DESKTOP-H7F119K.localdomain>
2024-04-19 21:46:18 -0500

    Revert "Begin word repo for db CRUD functionality"
    This reverts commit e403a2507243efbd04e5bd8c8ef41bae7b6ee89b.

--- src/main/java/com/personalproject/spellingbee/model/Word.java
+++
@@ -1,6 +0,0 @@
-package com.personalproject.spellingbee.model;
-
-public class Word {
-    private Long wordId;
-    private String correctWord;
-}
--- src/main/java/com/personalproject/spellingbee/repository/WordRepository.java
+++
@@ -1,7 +0,0 @@
-package com.personalproject.spellingbee.repository;
-import com.personalproject.spellingbee.model.Word;
-import org.springframework.data.repository.CrudRepository;
-
-public interface WordRepository extends CrudRepository<Word, Long> {
-
-}
