be07a4c2023104a96801bf621648fc152a60091c
Connor Stack <connorjstack@DESKTOP-H7F119K.localdomain>
2024-04-19 22:01:38 -0500

    Create WordRepository CRUD interface

---
+++ src/main/java/com/personalproject/spellingbee/repository/WordRepository.java
@@ -0,0 +1,6 @@
+package com.personalproject.spellingbee.repository;
+import com.personalproject.spellingbee.Word;
+import org.springframework.data.repository.CrudRepository;
+
+public interface WordRepository extends CrudRepository<Word, Long>{
+}
