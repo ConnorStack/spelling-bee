da72a293f08fb49b41efec9c9b7c32d69d2e9b0c
Connor Stack <connorjstack@DESKTOP-H7F119K.localdomain>
2024-04-19 21:47:55 -0500

    Revert "Prepare token"
    This reverts commit 81dfd59a8ea4062aec0c455a503261e02d7c677f.

--- src/main/java/com/personalproject/spellingbee/SpellingbeeApplication.java
+++ src/main/java/com/personalproject/spellingbee/SpellingbeeApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpellingbeeApplication {
-	//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
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpellingbeeApplication.class, args);
 	}
